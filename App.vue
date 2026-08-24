e206f377289f8044f4897f0f9dcc7248221684c2
69319010034-cloud <69319010034@udontech.ac.th>
2026-08-24 16:36:42 +0700

    round2: add student name

--- App.vue
+++ App.vue
@@ -1,16 +1,24 @@
 <template>
-  <div class="container">
-    <h1>Final Test Project</h1>
-  </div>
+<div class="container">
+<h1>{{ studentName }}</h1>
+</div>
 </template>
-
+<script>
+export default {
+data() {
+return {
+studentName: "กมลชนก ศรสันติ์" // แก้เป็นชื่อของตนเอง
+};
+},
+};
+</script>
 <style>
 .container {
-  width: 100vw;
-  height: 100vh;
-  background-color: skyblue; /* สีฟ้าตามโจทย์ */
-  display: flex;
-  justify-content: center;
-  align-items: center;
+width: 100vw;
+height: 100vh;
+background-color: skyblue;
+display: flex;
+align-items: center;
+justify-content: center;
 }
 </style>
