a89d67e56a6a7d397d1d9b60247de601199adf46
69319010034-cloud <69319010034@udontech.ac.th>
2026-08-24 15:55:31 +0700

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <h1>Final Test Project</h1>
+  </div>
+</template>
+
+<style>
+.container {
+  width: 100vw;
+  height: 100vh;
+  background-color: skyblue; /* สีฟ้าตามโจทย์ */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
